3584da472f055f26b90a610f0589f021e7ea645e
francescoP02 <file.partipilo@gmail.com>
2022-06-29 10:00:44 +0200

    Database connected and display data

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Movie;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index() {
+        $movies = Movie::all();
+        // dd($movies);
+        return view('homepage', compact('movies'));
+    }
+}
